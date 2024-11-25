029e9dfe46f29854843b4761188bf28c1af87996
ShavindiSanthupamaLiyanage <shavis1113@gmail.com>
2024-11-25 19:17:14 +0530

    Initial Commit

---
+++ EventTicketingSystem/src/main/java/backend/EventTicketingSystem/configurations/SwaggerConfiguration.java
@@ -0,0 +1,58 @@
+package backend.EventTicketingSystem.configurations;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfiguration implements WebMvcConfigurer {
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//                .apis(RequestHandlerSelectors.basePackage("com.shavi.RealTimeEventTicketingSystem"))
+//                .paths(PathSelectors.regex("/.*"))
+//                .build().apiInfo(apiInfoMetaData());
+//    }
+//    private ApiInfo apiInfoMetaData() {
+//        return new ApiInfoBuilder().title("Event Ticketing System")
+//                .description("API Endpoint Decoration")
+//                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+//                .license("Apache 2.0")
+//                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+//                .version("1.0.0")
+//                .build();
+//    }
+//}
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Real-Time Event Ticketing System API").version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Enter JWT token")));
+    }
+}
+
